a19a346fa26c809b03b19a1b5612877ce6cbb4d7
sudebaker <jcifuentesfonseca@gmail.com>
2023-09-08 12:06:51 +0200

    Cambios estéticos

--- watch_battery.py
+++ watch_battery.py
@@ -8,8 +8,8 @@ import dbus
 
 class batState():
     """Controls battery percentage,state and daemon profiles """
-    
-    #sudo su -c 'echo 12 > /sys/class/backlight/acpi_video0/brightness'
+
+    # sudo su -c 'echo 12 > /sys/class/backlight/acpi_video0/brightness'
 
     MIN_BAT_TRIGGER = 30
     MAX_BAT_TRIGGER = 80
@@ -17,17 +17,17 @@ class batState():
     def __init__(self) -> None:
         self.sys_bus = dbus.SystemBus()
         self.bat0_object = self.sys_bus.get_object(
-                'org.freedesktop.UPower','/org/freedesktop/UPower/devices/battery_BAT0'
-            )
+            'org.freedesktop.UPower', '/org/freedesktop/UPower/devices/battery_BAT0'
+        )
         self.bat0 = dbus.Interface(
-                self.bat0_object, 'org.freedesktop.DBus.Properties'
-            )
+            self.bat0_object, 'org.freedesktop.DBus.Properties'
+        )
         item = "org.freedesktop.Notifications"
         self.notfy_intf = dbus.Interface(
             dbus.SessionBus().get_object(item, "/"+item.replace(".", "/")), item
         )
-        self.pwd = self.sys_bus.get_object("net.hadess.PowerProfiles","/net/hadess/PowerProfiles")
-        self.pwd_interface = dbus.Interface(self.pwd,"org.freedesktop.DBus.Properties")
+        self.pwd = self.sys_bus.get_object("net.hadess.PowerProfiles", "/net/hadess/PowerProfiles")
+        self.pwd_interface = dbus.Interface(self.pwd, "org.freedesktop.DBus.Properties")
         #####
         self.active_profile = None
         self.ps_profile = "power-saver"
@@ -40,30 +40,32 @@ class batState():
 
     def get_state(self) -> None:
         state = self.bat0.Get("org.freedesktop.UPower.Device", "State")
-        if state == 1: # power ac
+        if state == 1:  # power ac
             self.state = "on_ac"
         elif state == 2:
             self.state = "on_battery"
 
-    def set_powerprofile(self, profile:str) -> None:
-        self.pwd_interface.Set("net.hadess.PowerProfiles","ActiveProfile", profile)
+    def set_powerprofile(self, profile: str) -> None:
+        self.pwd_interface.Set("net.hadess.PowerProfiles", "ActiveProfile", profile)
 
     def get_powerprofile(self):
-        active_profile = self.pwd_interface.Get("net.hadess.PowerProfiles","ActiveProfile")
+        active_profile = self.pwd_interface.Get("net.hadess.PowerProfiles", "ActiveProfile")
         self.active_profile = active_profile.split(",")[0]
-    
-    def notify (self,message:str) -> None:
+
+    def notify(self, message: str) -> None:
         self.notfy_intf.Notify(
             "", 0, "battery", "Battery Notification", f"{message}",
             [], {"critical": 1}, 5000
         )
-        
-def watch_battery(time_to_sleep:int=5,profile:str="balanced") -> None:
+
+
+def watch_battery(time_to_sleep: int = 5, profile: str = "balanced") -> None:
     """seconds to sleep and default power-profile"""
 
-    #getting current state
     bat_stat = batState()
+    # Main loop
     while True:
+        # getting current state
         bat_stat.get_powerprofile()
         # check for power status and adjust powerprofiles
         if bat_stat.state == "on_battery" and bat_stat.active_profile == bat_stat.bc_profile:
@@ -87,5 +89,6 @@ def watch_battery(time_to_sleep:int=5,profile:str="balanced") -> None:
         bat_stat.get_percentage()
         bat_stat.get_state()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     watch_battery(time_to_sleep=10)
